312071b9a0f17ddcb16eabc7d338f797c81eada4
bernst4315 <bernst4315@gmail.com>
2025-03-16 12:38:50 -0600

    have the ability to add new decks

--- frontend/src/pages/Decks.jsx
+++ frontend/src/pages/Decks.jsx
@@ -1,24 +1,28 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import Button from "../components/Button";
 
-
-//array of decks which is an array of flashcard objs
-//deck will be an obj with an array of cards
-
 export default function Decks(){
-    const decks = [];
-    const deck = {
-        title: "deck one",
-        cards: []
-    }; 
 
-    decks.push(deck)
+    const [decks, setDecks] = useState(null)
+
+    async function getDecks(){
+        const decks = await axios.get("http://localhost:3000/api/decks"); 
+        setDecks(decks.data)
+    }
+
+    useEffect(() =>{
+        getDecks();
+
+    }, [])
 
     return(
      <>
         <h1>Your Decks</h1>
-        {decks.map((deck) => (
-            <button>{deck.title}</button>
-        ))}
+        {decks && decks.map((deck) => {
+            return <button>{deck.title}</button>
+        })}
         
         <Button btnName="+Deck" linkRef="/decks/newdeck" />
      </>
