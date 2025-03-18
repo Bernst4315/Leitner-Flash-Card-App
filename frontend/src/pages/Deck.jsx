4dbcf095ee1e0e20acaa3955b1317ea291fdbdea
bernst4315 <bernst4315@gmail.com>
2025-03-17 18:35:41 -0600

    added a toggle function to cards

--- frontend/src/pages/Deck.jsx
+++ frontend/src/pages/Deck.jsx
@@ -4,19 +4,24 @@ import axios from "axios";
 
 
 import CreateFlashcard from "../components/CreateFlashcard";
+import Flashcard from "../components/Flashcard";
 
 export default function Deck (){
 
     //will need to create a patch request to deck in order to add cards
 
     const [deckTitle, setDeckTitle] = useState(null)
+    const [deck, setDeck] = useState(null)
+    const [flip, setFlip] = useState(false)
 
     const { id } = useParams(); 
 
     async function getDeckById () {
         const deck = await axios.get(`http://localhost:3000/api/decks/${id}`)
-        console.log(deck.data)
+        console.log("from get deck by id")
+        console.log(deck.data.cards)
         setDeckTitle(deck.data.title)
+        setDeck(deck.data.cards)
     }
 
     async function getFlashcard(newFlashcard){
@@ -24,6 +29,7 @@ export default function Deck (){
         //const flashCard = newFlashcard;
         const deck = await axios.patch(`http://localhost:3000/api/decks/${id}`, { $push: {cards: newFlashcard}})
         console.log("from getFlashcard")
+        console.log(deck.data)
         console.log(newFlashcard)
         //return newFlashcard; 
 }
@@ -33,10 +39,19 @@ export default function Deck (){
 
     }, [])
 
+    function toggle(flashcardID){
+        
+        setFlip((prevFlip) => ({...prevFlip, [flashcardID]: !prevFlip[flashcardID]}))
+    
+    }
+
     return (
         <div>
             {deckTitle && <h3>{deckTitle}</h3>}
             <CreateFlashcard getFlashcard={getFlashcard} />
+            {deck && deck.map((flashcard) => (
+                <p key={flashcard._id} className="flashcard border" onClick={() => toggle(flashcard._id)}>{flip[flashcard._id] ? flashcard.front : flashcard.back}</p>
+            ))}
             <Link to="/decks">
                 <button>Decks</button>
             </Link>
